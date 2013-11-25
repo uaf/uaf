21a72dc8d7a2b85fba02caff612006ee14e46d21
Wim Pessemier <wimpe@ster.kuleuven.be>
2013-11-25 10:40:27 +0100

    added async examples

--- examples/pyuaf/client/how_to_read_some_data.py
+++ examples/pyuaf/client/how_to_read_some_data.py
@@ -7,12 +7,18 @@ A UAF Client can read data in two ways:
   - either using the "convenience" method: read()
   - or using the "generic" method: processRequest() 
 
-The "convenience" methods (such as read(), write(), call(), ...) are conveniently to use 
+The "convenience" methods (such as read(), beginRead(), write(), ...) are conveniently to use 
  (since they accept the most frequently used parameters directly), 
 but they are less powerful than the "generic" processRequest() method 
- (since this method accepts ReadRequests, WriteRequests, etc. that can be fully configured).
+ (since this method accepts ReadRequests, AsyncReadRequests, etc. that can be fully configured).
 
-In this example, we will use both ways to read data.
+In this example, we will use several ways to read data:
+ - first option  : "convenience" synchronous  function read()
+ - second option : "generic"     synchronous  function processRequest()
+ - third option  : "convenience" asynchronous function beginRead()       , with external callback
+ - fourth option : "generic"     asynchronous function processRequest()  , with external callback
+ - fifth option  : "convenience" asynchronous function beginRead()       , with overridden callback
+ - sixth option  : "generic"     asynchronous function processRequest()  , with overridden callback
 
 To run the example, start the UaDemoServer of UnifiedAutomation first on the same machine. 
 """
@@ -22,7 +28,7 @@ import time, os
 import pyuaf
 from pyuaf.client           import Client
 from pyuaf.client.settings  import ClientSettings
-from pyuaf.client.requests  import ReadRequest, ReadRequestTarget
+from pyuaf.client.requests  import ReadRequest, AsyncReadRequest, ReadRequestTarget 
 from pyuaf.client.configs   import ReadConfig, SessionConfig
 from pyuaf.util             import Address, NodeId, RelativePathElement, QualifiedName, LocalizedText
 from pyuaf.util             import primitives
@@ -92,23 +98,23 @@ myClient = Client(settings)
 
 
 # read the node attributes all at once
+print("")
+print("First option: use the convenience function read() to read data synchronously")
+print("============================================================================")
+    
+# OPTIONAL: You could also provide a ReadConfig to configure a call timeout, 
+#           or maximum age of the values, or ... 
+serviceConfig = ReadConfig()
+serviceConfig.serviceSettings.callTimeoutSec = 0.5
+serviceConfig.serviceSettings.maxAgeSec = 1.0
+
+# OPTIONAL: And you could also provide a SessionConfig to configure the sessions 
+#           (e.g. set the timeout to 600.0 seconds)
+#           For more info about SessionConfigs, see the sessionconfig_example
+sessionConfig = SessionConfig()
+sessionConfig.defaultSessionSettings.sessionTimeoutSec = 600.0
+
 try:
-    print("")
-    print("First option: use the convenience function \"read()\"")
-    print("===================================================")
-    
-    # OPTIONAL: You could also provide a ReadConfig to configure a call timeout, 
-    #           or maximum age of the values, or ... 
-    serviceConfig = ReadConfig()
-    serviceConfig.serviceSettings.callTimeoutSec = 0.5
-    serviceConfig.serviceSettings.maxAgeSec = 1.0
-    
-    # OPTIONAL: And you could also provide a SessionConfig to configure the sessions 
-    #           (e.g. set the timeout to 600.0 seconds)
-    #           For more info about SessionConfigs, see the sessionconfig_example
-    sessionConfig = SessionConfig()
-    sessionConfig.defaultSessionSettings.sessionTimeoutSec = 600.0
-    
     # now read the node attributes
     readResult = myClient.read( addresses     = [someDoubleNode, someUInt32Node, someStringNode, 
                                                  someLocalizedTextNode, someSByteArrayNode],
@@ -117,33 +123,38 @@ try:
     
     # print the result using the function we defined before
     printResult(readResult)
-    
-    print("")
-    print("Second option: use the generic function \"processRequest()\"")
-    print("==========================================================")
-    
-    # create a request with 5 targets
-    readRequest = ReadRequest(5)
-    readRequest.targets[0].address = someDoubleNode
-    readRequest.targets[1].address = someUInt32Node
-    readRequest.targets[2].address = someStringNode
-    readRequest.targets[3].address = someLocalizedTextNode
-    readRequest.targets[4].address = someSByteArrayNode
-    
-    # we could also add a 6th node, of which we want to read the DisplayName (= of type LocalizedText)
-    readRequest.targets.append(ReadRequestTarget()) # or readRequest.targets.resize(6)
-    readRequest.targets[5].address     = someDoubleNode
-    readRequest.targets[5].attributeId = pyuaf.util.attributeids.DisplayName
-    
-    # OPTIONAL: let's also specify a small call timeout, since the UaDemoServer is running 
-    #           on the local machine anyway
-    readRequest.serviceConfig.serviceSettings.callTimeoutSec = 0.5
-    # OPTIONAL: let's also specify that the values cannot be older than 1 second
-    readRequest.serviceConfig.serviceSettings.maxAgeSec = 1.0
-    # OPTIONAL: and finally let's also specify that sessions should have a timeout of 600 seconds
-    #           For more info about SessionConfigs, see the sessionconfig_example
-    readRequest.sessionConfig.defaultSessionSettings.sessionTimeoutSec = 600.0
-    
+except UafError, e:
+    print("Some error occurred: %s" %e)
+
+
+
+print("")
+print("Second option: use the generic function processRequest() to read data synchronously")
+print("===================================================================================")
+
+# create a request with 5 targets
+readRequest = ReadRequest(5)
+readRequest.targets[0].address = someDoubleNode
+readRequest.targets[1].address = someUInt32Node
+readRequest.targets[2].address = someStringNode
+readRequest.targets[3].address = someLocalizedTextNode
+readRequest.targets[4].address = someSByteArrayNode
+
+# we could also add a 6th node, of which we want to read the DisplayName (= of type LocalizedText)
+readRequest.targets.append(ReadRequestTarget()) # or readRequest.targets.resize(6)
+readRequest.targets[5].address     = someDoubleNode
+readRequest.targets[5].attributeId = pyuaf.util.attributeids.DisplayName
+
+# OPTIONAL: let's also specify a small call timeout, since the UaDemoServer is running 
+#           on the local machine anyway
+readRequest.serviceConfig.serviceSettings.callTimeoutSec = 0.5
+# OPTIONAL: let's also specify that the values cannot be older than 1 second
+readRequest.serviceConfig.serviceSettings.maxAgeSec = 1.0
+# OPTIONAL: and finally let's also specify that sessions should have a timeout of 600 seconds
+#           For more info about SessionConfigs, see the sessionconfig_example
+readRequest.sessionConfig.defaultSessionSettings.sessionTimeoutSec = 600.0
+
+try:    
     # process the request
     readResult = myClient.processRequest(readRequest)
     
@@ -155,10 +166,145 @@ try:
     data   = readResult.targets[5].data
     if status.isGood() and isinstance(data, LocalizedText):
         print("The DisplayName-locale is: '%s', the DisplayName-text is: '%s'" %(data.locale(), data.text()))
+except UafError, e:
+    print("Some error occurred: %s" %e)
     
+    
+
+print("")
+print("Third option: use the convenience function beginRead() to read data asynchronously")
+print("==================================================================================")
+
+# notice we enable asynchronous communication now by providing the printResult() method as 
+# a callback function!
+
+try:
+    asyncReadResult = myClient.beginRead(addresses     = [someDoubleNode, someUInt32Node, 
+                                                          someStringNode, someLocalizedTextNode, 
+                                                          someSByteArrayNode],
+                                         serviceConfig = serviceConfig, # optional argument
+                                         sessionConfig = sessionConfig, # optional argument
+                                         callback      = printResult) # callback function!
+    
+    # check if there was an "immediate" error on the client side when executing the asynchronous 
+    # service call:
+    if asyncReadResult.overallStatus.isGood():
+        print("OK, the client could invoke the asynchronous request without problems.")
+        print("Let's wait a second for the result ...")
+        
+        time.sleep(1)
+        
+        print("The read result should have been received by now!")
+
+except UafError, e:
+    print("Some error occurred: %s" %e)
+
+
+print("")
+print("Fourth option: use the generic function processRequest() to read data asynchronously")
+print("====================================================================================")
+
+# notice we enable asynchronous communication now by providing the printResult() method as 
+# a callback function!
+
+# create an asynchronous (!) request with 5 targets
+asyncReadRequest = AsyncReadRequest(5)
+
+# we can simply copy the targets of the synchronous request to the asynchronous request:
+for i in xrange(5):
+    asyncReadRequest.targets[i] = readRequest.targets[i]
+    
+# OPTIONAL: also copy the service config and session config:
+asyncReadRequest.serviceConfig = readRequest.serviceConfig
+asyncReadRequest.sessionConfig = readRequest.sessionConfig
+
+try:
+    # process the request
+    asyncReadResult = myClient.processRequest(asyncReadRequest, resultCallback=printResult)
+    
+    # check if there was an "immediate" error on the client side when executing the asynchronous 
+    # service call:
+    if asyncReadResult.overallStatus.isGood():
+        print("OK, the client could invoke the asynchronous request without problems.")
+        print("Let's wait a second for the result ...")
+        
+        time.sleep(1)
+        
+        print("The read result should have been received by now!")
+
     
 except UafError, e:
     print("Some error occurred: %s" %e)
 
 
+
+print("")
+print("Fifth option: read data asynchronously using beginRead() without specifying external callbacks")
+print("==============================================================================================")
+
+# instead of using "external" callback functions (such as printResult() above), we can also use
+# the "internal" callback function of the Client class. If the argument 'callback' of beginRead() is 
+# not used (or 'resultCallback' is not used in case of processRequest()), the asynchronous result
+# will be dispatched to the readComplete() method of the Client class. 
+# In other words, when you subclass Client, you can simply override the readComplete() method
+# to receive the asynchronous read results.
+
+class MySubClasssedClient(Client):
+    
+    def __init__(self, settings):
+        Client.__init__(self, settings)
+
+    def readComplete(self, result): # overridden method!
+        print("This is the overridden method readComplete() speaking:")
+        printResult(result)
+
+# create the client
+mySubClassedClient = MySubClasssedClient(settings)
+    
+try:
+    asyncReadResult = mySubClassedClient.beginRead(
+                                         addresses     = [someDoubleNode, someUInt32Node, 
+                                                          someStringNode, someLocalizedTextNode, 
+                                                          someSByteArrayNode],
+                                         serviceConfig = serviceConfig, # optional argument
+                                         sessionConfig = sessionConfig) # optional argument!
+    # note: the 'callback' argument is not used in the above call!
+    
+    # check if there was an "immediate" error on the client side when executing the asynchronous 
+    # service call:
+    if asyncReadResult.overallStatus.isGood():
+        print("OK, the client could invoke the asynchronous request without problems.")
+        print("Let's wait a second for the result ...")
+        
+        time.sleep(1)
+        
+        print("The read result should have been received by now!")
+    
+except UafError, e:
+    print("Some error occurred: %s" %e)
+    
+
+
+print("")
+print("Sixth option: read data asynchronously using processRequest() without specifying external callbacks")
+print("===================================================================================================")
+
+try:
+    # process the request
+    asyncReadResult = mySubClassedClient.processRequest(asyncReadRequest) 
+    # note: the 'resultCallback' argument is not used in the above call!
+    
+    # check if there was an "immediate" error on the client side when executing the asynchronous 
+    # service call:
+    if asyncReadResult.overallStatus.isGood():
+        print("OK, the client could invoke the asynchronous request without problems.")
+        print("Let's wait a second for the result ...")
+        
+        time.sleep(1)
+        
+        print("The read result should have been received by now!")
+
+except UafError, e:
+    print("Some error occurred: %s" %e)
+    
 
