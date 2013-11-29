81e4685b56f902c0fb00576acf28a78ffcfee341
Wim Pessemier <wimpe@ster.kuleuven.be>
2013-11-29 17:13:07 +0100

    now supporting KeepAlive notifications

---
+++ src/uaf/client/subscriptions/keepalivenotification.cpp
@@ -0,0 +1,70 @@
+/* This file is part of the UAF (Unified Architecture Framework) project.
+ *
+ * Copyright (C) 2012 Wim Pessemier (Institute of Astronomy, KULeuven)
+ *
+ * Project website: http://www.ster.kuleuven.be/uaf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "uaf/client/subscriptions/keepalivenotification.h"
+
+
+
+
+
+namespace uafc
+{
+    using namespace uaf;
+    using namespace uafc;
+    using std::string;
+    using std::stringstream;
+    using std::vector;
+
+
+    // Constructor
+    // =============================================================================================
+    KeepAliveNotification::KeepAliveNotification()
+    : SubscriptionInformation()
+    {}
+
+
+    // Get a string representation
+    // =============================================================================================
+    string KeepAliveNotification::toString(const string& indent, size_t colon) const
+    {
+        stringstream ss;
+
+        ss << SubscriptionInformation::toString(indent, colon) << "\n";
+
+        ss << indent << " - notificationHandles";
+        ss << fillToPos(ss, colon);
+        ss << ": [";
+        for(size_t i = 0; i < notificationHandles.size(); i++)
+        {
+            ss << notificationHandles[i];
+            // add a comma unless we're printing the last handle
+            if (i != notificationHandles.size() - 1)
+                ss << ",";
+        }
+        ss << "]";
+
+        return ss.str();
+    }
+
+}
+
+
+
+
---
+++ src/uaf/client/subscriptions/keepalivenotification.h
@@ -0,0 +1,80 @@
+/* This file is part of the UAF (Unified Architecture Framework) project.
+ *
+ * Copyright (C) 2012 Wim Pessemier (Institute of Astronomy, KULeuven)
+ *
+ * Project website: http://www.ster.kuleuven.be/uaf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UAFC_KEEPALIVENOTIFICATION_H_
+#define UAFC_KEEPALIVENOTIFICATION_H_
+
+
+// STD
+#include <vector>
+#include <string>
+#include <sstream>
+// SDK
+// UAF
+#include "uaf/util/status.h"
+#include "uaf/util/variant.h"
+#include "uaf/util/logger.h"
+#include "uaf/util/address.h"
+#include "uaf/util/stringifiable.h"
+#include "uaf/client/clientexport.h"
+#include "uaf/util/handles.h"
+#include "uaf/client/subscriptions/basenotification.h"
+#include "uaf/client/subscriptions/subscriptioninformation.h"
+
+
+namespace uafc
+{
+
+    /*******************************************************************************************//**
+    * A uafc::KeepAliveNotification is the notification that a subscription is still alive.
+    *
+    * @ingroup ClientSubscriptions
+    ***********************************************************************************************/
+    class UAFC_EXPORT KeepAliveNotification : public uafc::SubscriptionInformation
+    {
+    public:
+
+        typedef std::vector<uaf::NotificationHandle> NotificationHandles;
+
+
+        /**
+         * Construct a keep alive notification.
+         */
+        KeepAliveNotification();
+
+
+        /** The notification handles assigned to the subscription that got a keep alive message. */
+        NotificationHandles notificationHandles;
+
+
+        /**
+         * Get a string representation of the notification.
+         */
+        std::string toString(const std::string& indent="", std::size_t colon=24) const;
+    };
+
+
+
+}
+
+
+
+
+#endif /* UAFC_KEEPALIVENOTIFICATION_H_ */
