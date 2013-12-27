ac4d7138d681d3448a583b0762d7291cc8721ae2
Wim Pessemier <wimpe@ster.kuleuven.be>
2013-12-27 09:36:41 +0100

    moved from UaDemoServer to uaservercpp

--- examples/pyuaf/client/easiest_client_example.py
+++ examples/pyuaf/client/easiest_client_example.py
@@ -1,20 +1,22 @@
 # examples/pyuaf/client/easiest_client_example.py
 
-# Start the UaDemoserver of Unified Automation before running this script!
+# Start the demo server ($SDK/bin/uaservercpp) of Unified Automation before running this script!
 
 import pyuaf
 from pyuaf.util             import Address, NodeId
 from pyuaf.client           import Client
 from pyuaf.client.settings  import ClientSettings
 
-# create a client named "myClient", and provide the discovery URL of the UaDemoserver:
-myClient = Client(ClientSettings("myClient", ["opc.tcp://localhost:4841"]))
+# create a client named "myClient", and provide the discovery URL of the server (uaservercpp):
+myClient = Client(ClientSettings("myClient", ["opc.tcp://localhost:48010"]))
 
 # specify the address of the node of which we would like to read its Value attribute:
 # (here we're using an absolute address, i.e. a NodeId(<identifier>, <namespace URI>) and a <server URI>)
-someAddress = Address(NodeId("MyDemoObject.Counter", "DemoNodeManager"), "urn:UnifiedAutomation:UaDemoserver")
+someAddress = Address(NodeId("Demo.SimulationSpeed",                         # NodeId identifier
+                             "http://www.unifiedautomation.com/DemoServer"), # NodeId namespace URI
+                      "urn:UnifiedAutomation:UaServerCpp")                   # server URI
 
 # read the Value attribute of the node that we just addressed:
 result = myClient.read( [someAddress] )
 
-print("The counter value is %d" %result.targets[0].data.value)
+print("The value is %d" %result.targets[0].data.value)
