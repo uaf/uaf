20c4c82f8dd215ce19d2a775514a005dbd045b76
Wim Pessemier <wimpe@ster.kuleuven.be>
2014-07-29 10:28:32 +0200

    Added unit tests for the previously added functionality

---
+++ unittests/pyuaftests/client/client_connectionstatus.py
@@ -0,0 +1,156 @@
+import pyuaf
+import time
+import thread
+import unittest
+from pyuaf.util.unittesting import parseArgs
+
+from pyuaf.util import NodeId, Address, ExpandedNodeId, BrowsePath, \
+                       RelativePathElement, QualifiedName, opcuaidentifiers
+from pyuaf.client.requests import ReadRequest, ReadRequestTarget
+from pyuaf.client.results  import ReadResult,  ReadResultTarget
+
+
+
+ARGS = parseArgs()
+
+
+def suite(args=None):
+    if args is not None:
+        global ARGS
+        ARGS = args
+    
+    return unittest.TestLoader().loadTestsFromTestCase(ClientConnectionStatusTest)
+
+
+class ConnectionStatusCallbackClass:
+    def __init__(self):
+        self.infoList = []
+    
+    def callback(self, info):
+        self.infoList.append(info)
+
+
+class MyClient(pyuaf.client.Client):
+
+    def __init__(self, settings):
+        pyuaf.client.Client.__init__(self, settings)
+        self.infoList = []
+    
+    def connectionStatusChanged(self, info):
+        self.infoList.append(info)
+
+
+class ClientConnectionStatusTest(unittest.TestCase):
+    
+    
+    def setUp(self):
+        
+        # create a new ClientSettings instance and add the localhost to the URLs to discover
+        settings = pyuaf.client.settings.ClientSettings()
+        settings.discoveryUrls.append(ARGS.demo_url)
+        settings.applicationName = "client"
+        settings.logToStdOutLevel = ARGS.loglevel
+        
+        self.client = MyClient(settings)
+    
+        self.serverUri = ARGS.demo_server_uri
+        demoNsUri = ARGS.demo_ns_uri
+        plcOpenNsUri = "http://PLCopen.org/OpcUa/IEC61131-3/"
+        
+        # define some nodeids
+        self.id = NodeId(opcuaidentifiers.OpcUaId_Server_Auditing, 0)
+        
+        # define some addresses
+        self.address = Address(ExpandedNodeId(self.id, self.serverUri))
+    
+
+    def test_client_Client_register_all_connectionstatuses_and_read_node(self):
+        cbc = ConnectionStatusCallbackClass()
+        self.client.registerConnectionStatusCallback(cbc.callback)
+        
+        result = self.client.read(self.address)
+        self.assertTrue( result.targets[0].status.isGood() )
+        # the callbacks should have been called once:
+        for source in [self.client, cbc]:
+            self.assertEqual( len(source.infoList), 1 )
+            self.assertEqual( source.infoList[0].serverUri, self.serverUri)
+            self.assertEqual( source.infoList[0].sessionState, pyuaf.client.sessionstates.Connected )
+        
+
+    def test_client_Client_register_all_connectionstatuses_and_manually_connect(self):
+        cbc = ConnectionStatusCallbackClass()
+        self.client.registerConnectionStatusCallback(cbc.callback)
+        
+        clientConnectionId = self.client.manuallyConnect(self.serverUri)
+        
+        # the callback will only be called after the session gets connected, so wait for this to happen!
+        t_end = time.time() + 1.0 # 1 second timeout
+        while time.time() < t_end and len(cbc.infoList) == 0:
+            pass # do nothing, just wait
+        
+        # the callbacks should have been called once:
+        for source in [self.client, cbc]:
+            self.assertEqual( len(source.infoList), 1 )
+            self.assertEqual( source.infoList[0].serverUri, self.serverUri)
+            self.assertEqual( source.infoList[0].sessionState, pyuaf.client.sessionstates.Connected )
+        
+
+    def test_client_Client_register_connectionstatus_for_serveruri_and_read_node(self):
+        cbc = ConnectionStatusCallbackClass()
+        self.client.registerConnectionStatusCallback(cbc.callback, onlyServerUri = self.serverUri)
+        
+        result = self.client.read(self.address)
+        self.assertTrue( result.targets[0].status.isGood() )
+        
+        # the callbacks should have been called once:
+        for source in [self.client, cbc]:
+            self.assertEqual( len(source.infoList), 1 )
+            self.assertEqual( source.infoList[0].serverUri, self.serverUri)
+            self.assertEqual( source.infoList[0].sessionState, pyuaf.client.sessionstates.Connected )
+        
+
+    def test_client_Client_register_connectionstatus_for_clientconnectionid_and_read_node(self):
+        cbc = ConnectionStatusCallbackClass()
+        # since we created a new client, the clientConnectionId will be 0:
+        clientConnectionId = 0
+        self.client.registerConnectionStatusCallback(cbc.callback, onlyClientConnectionId = clientConnectionId)
+        
+        result = self.client.read(self.address)
+        self.assertTrue( result.targets[0].status.isGood() )
+        # the callbacks should have been called once:
+        for source in [self.client, cbc]:
+            self.assertEqual( len(source.infoList), 1 )
+            self.assertEqual( cbc.infoList[0].clientConnectionId, clientConnectionId)
+            self.assertEqual( source.infoList[0].serverUri, self.serverUri)
+            self.assertEqual( source.infoList[0].sessionState, pyuaf.client.sessionstates.Connected )
+        
+
+    def test_client_Client_register_connectionstatus_in_several_ways_and_manually_connect(self):
+        cbc0 = ConnectionStatusCallbackClass()
+        self.client.registerConnectionStatusCallback(cbc0.callback)
+        cbc1 = ConnectionStatusCallbackClass()
+        self.client.registerConnectionStatusCallback(cbc1.callback, onlyServerUri = self.serverUri)
+        cbc2 = ConnectionStatusCallbackClass()
+        # since we created a new client, the clientConnectionId will be 0:
+        clientConnectionId = 0
+        self.client.registerConnectionStatusCallback(cbc2.callback, onlyClientConnectionId = clientConnectionId)
+        
+        result = self.client.read(self.address)
+        self.assertTrue( result.targets[0].status.isGood() )
+        # the callbacks should have been called once:
+        for source in [self.client, cbc0, cbc1, cbc2 ]:
+            self.assertEqual( len(source.infoList), 1 )
+            self.assertEqual( source.infoList[0].clientConnectionId, clientConnectionId)
+            self.assertEqual( source.infoList[0].serverUri, self.serverUri)
+            self.assertEqual( source.infoList[0].sessionState, pyuaf.client.sessionstates.Connected )
+    
+    
+    def tearDown(self):
+        # delete the client instances manually (now!) instead of letting them be garbage collected 
+        # automatically (which may happen during a another test, and which may cause logging output
+        # of the destruction to be mixed with the logging output of the other test).
+        del self.client
+        
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = ARGS.verbosity).run(suite())
---
+++ unittests/pyuaftests/client/client_keepalive.py
@@ -0,0 +1,99 @@
+import pyuaf
+import time
+import thread
+import unittest
+from pyuaf.util.unittesting import parseArgs
+
+from pyuaf.util import NodeId, Address, ExpandedNodeId, BrowsePath, \
+                       RelativePathElement, QualifiedName, opcuaidentifiers
+from pyuaf.client.requests import ReadRequest, ReadRequestTarget
+from pyuaf.client.results  import ReadResult,  ReadResultTarget
+
+
+
+ARGS = parseArgs()
+
+
+def suite(args=None):
+    if args is not None:
+        global ARGS
+        ARGS = args
+    
+    return unittest.TestLoader().loadTestsFromTestCase(ClientKeepAliveTest)
+
+
+class CallbackClass:
+    def __init__(self):
+        self.notificationList = []
+    
+    def callback(self, notification):
+        self.notificationList.append(notification)
+
+
+class MyClient(pyuaf.client.Client):
+
+    def __init__(self, settings):
+        pyuaf.client.Client.__init__(self, settings)
+        self.notificationList = []
+    
+    def keepAliveReceived(self, notification):
+        self.notificationList.append(notification)
+
+
+class ClientKeepAliveTest(unittest.TestCase):
+    
+    
+    def setUp(self):
+        
+        # create a new ClientSettings instance and add the localhost to the URLs to discover
+        settings = pyuaf.client.settings.ClientSettings()
+        settings.discoveryUrls.append(ARGS.demo_url)
+        settings.applicationName = "client"
+        settings.logToStdOutLevel = ARGS.loglevel
+        
+        self.client = MyClient(settings)
+    
+        self.serverUri = ARGS.demo_server_uri
+        demoNsUri = ARGS.demo_ns_uri
+        plcOpenNsUri = "http://PLCopen.org/OpcUa/IEC61131-3/"
+        
+        # define some nodeids
+        self.id = NodeId(opcuaidentifiers.OpcUaId_Server_Auditing, 0)
+        
+        # define some addresses
+        self.address = Address(ExpandedNodeId(self.id, self.serverUri))
+    
+    
+    def test_client_Client_register_keepalivecallback_and_monitor_static_node(self):
+        cbc = CallbackClass()
+        self.client.registerKeepAliveCallback(cbc.callback)
+        
+        # create a request that will certainly result in KeepAlive notifications
+        # It seems that we cannot force the UaServerCpp demo server to send KeepAlive messages
+        # faster than once every 5 seconds, so we keep it at that: 
+        request = pyuaf.client.requests.CreateMonitoredDataRequest(1)
+        request.targets[0].address = self.address
+        request.subscriptionConfig.defaultSubscriptionSettings.publishingIntervalSec = 1.0
+        request.subscriptionConfig.defaultSubscriptionSettings.maxKeepAliveCount = 5
+        
+        result = self.client.processRequest(request)
+        self.assertTrue( result.targets[0].status.isGood() )
+        
+        # wait some time
+        time.sleep(7.0)
+        
+        # the callbacks should have been called at least once:
+        for source in [self.client, cbc]:
+            self.assertTrue( len(source.notificationList) >= 1 )
+            self.assertEqual( source.notificationList[0].subscriptionState, pyuaf.client.subscriptionstates.Created )
+     
+
+    def tearDown(self):
+        # delete the client instances manually (now!) instead of letting them be garbage collected 
+        # automatically (which may happen during a another test, and which may cause logging output
+        # of the destruction to be mixed with the logging output of the other test).
+        del self.client
+        
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = ARGS.verbosity).run(suite())
