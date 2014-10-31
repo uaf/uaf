ea1da7028ad8794618639d7ba33348903b895c00
Wim Pessemier <wimpe@ster.kuleuven.be>
2014-10-31 22:35:43 +0100

    added some extra documentation

--- examples/pyuaf/client/how_to_connect_to_a_secured_endpoint.py
+++ examples/pyuaf/client/how_to_connect_to_a_secured_endpoint.py
@@ -18,16 +18,43 @@ DEMOSERVER_NS_URI = "http://www.unifiedautomation.com/DemoServer"
 PKI_FOLDER = "./securityexample/PKI"
 
 
-print("")
-print("To run this example, start the UaServerCpp demo server first.")
-print("If you use the evaluation SDK, you can find the executable here:")
-print("   C:/Program Files/UnifiedAutomation/UaSdkCppBundleEval/bin/uaservercpp.exe")
-print("")
-print("Note that all paths below must ALWAYS be specified using '/' separators, also on Windows!")
-print("This means you cannot use e.g. os.path.join or similar, since those functions may")
-print("introduce platform-dependent path separators.")
 
 
+
+description = """
+To run this example, start the UaServerCpp demo server first.
+If you use the evaluation SDK, you can find thCliente executable here (e.g. on Windows):
+   C:/Program Files/UnifiedAutomation/UaSdkCppBundleEval/bin/uaservercpp.exe
+Note that all paths below must ALWAYS be specified using '/' separators, also on Windows!
+This means you cannot use e.g. os.path.join or similar, since those functions may
+introduce platform-dependent path separators.
+
+Here's how the UAF Client handles certificates:
+1) first the server certificate is verified. 
+   This server certificate was automatically received and kept in memory by the UAF
+   client in the background during the discovery process, so you don't have to take 
+   care of it yourself. (Note, that if you use ``manuallyConnectToEndpoint`` to avoid 
+   discovery for some reason, you may provide the server certificate manually). 
+   If the server certificate is not trusted (e.g. because it's invalid or because 
+   it's not in the trust list), then the ``untrustedServerCertificateReceived`` 
+   will be called. This is done **regardless** of whether or not you want to sign or
+   encrypt your communication! In this step you're **only** checking if you're trying
+   to connect to the right server. In principle you should always override the 
+   ``untrustedServerCertificateReceived`` method for this purpose (or register
+   your own callback via ``registerUntrustedServerCertificateCallback``). 
+   By default pyuaf will simply assume that you trust the server to which you're
+   trying to connect (i.e. ``untrustedServerCertificateReceived`` returns 
+   ``Action_AcceptTemporarily`` unless you override it). 
+2) then both the client and server certificates are sent around again:
+   - so that the server can decide to accept or reject the session
+   - and so that the actual communication data can be encrypted and/or signed, 
+     if needed.
+   All of this is handled by the UAF and SDK behind the scenes, so you don't have to 
+   take care of it yourself. Basically you only have to configure the client
+   settings and session settings. 
+   
+"""
+print(description)
 
 print("")
 print("===========================================================================================")
