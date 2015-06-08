cd6330df53dde7c59618aee7079dcbd4ff6bfd48
robi-wan <robi-wan@suyu.de>
2015-06-08 23:05:42 +0200

    Tweak helper scripts to prepend uaf\lib directory to environment variables
    Prepending the uaf\lib directory assures that the correct libraries are
    found e.g. for executing the tests.
    
    I had an entry in the PATH environment variable which pointed to an OPC
    UA SDK 1.3.3 - that caused the error "ImportError: DLL load failed: The
    specified procedure could not be found." when running the tests. Tests
    on master require OPC UA SDK 1.4.x.

--- unittests/uaf-commandprompt.bat
+++ unittests/uaf-commandprompt.bat
@@ -2,14 +2,14 @@
 
 echo This Windows script (uaf-commandprompt.bat) should be executed in Command prompt
 echo and does the following:
-echo  - it appends the uaf\lib directory to the PATH environment variable
-echo  - it appends the uaf\lib directory to the PYTHONPATH environment variable
+echo  - it prepends the uaf\lib directory to the PATH environment variable
+echo  - it prepends the uaf\lib directory to the PYTHONPATH environment variable
 echo
 :: =============================================================================
 
 cd /d %~dp0
-set PATH=%PATH%;%~dp0..\lib
-set PYTHONPATH=%PYTHONPATH%;%~dp0..\lib
+set PATH=%~dp0..\lib;%PATH%
+set PYTHONPATH=%~dp0..\lib;%PYTHONPATH%
 
 echo.
 echo New PATH: 
