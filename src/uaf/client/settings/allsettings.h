77e23a915cd9721b6981671fc2a33e2b8eb17f7d
Wim Pessemier <wimpe@ster.kuleuven.be>
2015-06-25 08:13:13 +0200

    added a file which includes all other settings

---
+++ src/uaf/client/settings/allsettings.h
@@ -0,0 +1,38 @@
+/* This file is part of the UAF (Unified Architecture Framework) project.
+ *
+ * Copyright (C) 2012 Wim Pessemier (Institute of Astronomy, KULeuven)
+ *
+ * Project website: http://www.ster.kuleuven.be/uaf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UAF_ALLSETTINGS_H_
+#define UAF_ALLSETTINGS_H_
+
+#include "uaf/client/clientexport.h"
+#include "uaf/client/settings/readsettings.h"
+#include "uaf/client/settings/writesettings.h"
+#include "uaf/client/settings/methodcallsettings.h"
+#include "uaf/client/settings/createmonitoreddatasettings.h"
+#include "uaf/client/settings/createmonitoredeventssettings.h"
+#include "uaf/client/settings/translatebrowsepathstonodeidssettings.h"
+#include "uaf/client/settings/browsesettings.h"
+#include "uaf/client/settings/browsenextsettings.h"
+#include "uaf/client/settings/historyreadrawmodifiedsettings.h"
+#include "uaf/client/settings/sessionsettings.h"
+#include "uaf/client/settings/subscriptionsettings.h"
+#include "uaf/client/settings/clientsettings.h"
+
+#endif /* UAF_ALLSETTINGS_H_ */
